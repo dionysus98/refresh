5d5261278f2df371b2ff070c6cdb3670ac84496b
Avinash S <avinashthebigboss@gmail.com>
2024-09-01 22:37:40 +0530

    haskell | blog-gen | better parser

--- haskell/blog-gen/Markup.hs
+++ haskell/blog-gen/Markup.hs
@@ -4,6 +4,7 @@ module Markup
   )
 where
 
+import Data.Maybe (maybeToList)
 import Numeric.Natural
 
 type Document = [Structure]
@@ -16,20 +17,65 @@ data Structure
   | CodeBlock [String]
   deriving (Show)
 
-parse :: String -> Document
-parse = parseLines [] . lines
+-- parse :: String -> Document
+-- parse = parseLines [] . lines
 
-parseLines :: [String] -> [String] -> Document
-parseLines curPara ipTxts =
-  let para = Paragraph $ unlines $ reverse curPara
-   in case ipTxts of
-        [] -> [para]
-        curLine : rest ->
-          if trim curLine == ""
-            then
-              para : parseLines [] rest
-            else
-              parseLines (curLine : curPara) rest
+-- parseLines :: [String] -> [String] -> Document
+-- parseLines curPara ipTxts =
+--   let para = Paragraph $ unlines $ reverse curPara
+--    in case ipTxts of
+--         [] -> [para]
+--         curLine : rest ->
+--           if trim curLine == ""
+--             then
+--               para : parseLines [] rest
+--             else
+--               parseLines (curLine : curPara) rest
 
 trim :: String -> String
 trim = unwords . words
+
+parse :: String -> Document
+parse = parseLines Nothing . lines
+
+parseLines :: Maybe Structure -> [String] -> Document
+-- Empty; Done case
+parseLines context [] = maybeToList context
+-- Heading 1
+parseLines context (('*' : ' ' : line) : rest) =
+  maybe id (:) context (Heading 1 (trim line) : parseLines Nothing rest)
+-- Unordered List
+parseLines context (('-' : ' ' : line) : rest) =
+  case context of
+    Just (UnorderedList unList) ->
+      parseLines (Just (UnorderedList (unList <> [trim line]))) rest
+    _ ->
+      maybe id (:) context (parseLines (Just (UnorderedList [trim line])) rest)
+-- Ordered List
+parseLines context (('#' : ' ' : line) : rest) =
+  case context of
+    Just (OrderedList oList) ->
+      parseLines (Just (OrderedList (oList <> [trim line]))) rest
+    _ ->
+      maybe id (:) context (parseLines (Just (OrderedList [trim line])) rest)
+-- Ordered List
+parseLines context (('>' : ' ' : line) : rest) =
+  case context of
+    Just (CodeBlock code) ->
+      parseLines (Just (CodeBlock (code <> [line]))) rest
+    _ ->
+      maybe id (:) context (parseLines (Just (CodeBlock [line])) rest)
+-- Paragraph case
+parseLines context (curLine : rest) =
+  let --
+      line = trim curLine
+   in --
+      if line == ""
+        then
+          maybe id (:) context $ parseLines Nothing rest
+        else --
+          case context of
+            Just (Paragraph para) ->
+              parseLines (Just (Paragraph (unwords [para, line]))) rest
+            _ ->
+              maybe id (:) context $ parseLines (Just $ Paragraph line) rest
--- haskell/blog-gen/play.hs
+++ haskell/blog-gen/play.hs
@@ -1,3 +1,6 @@
+import Data.Maybe (listToMaybe, isNothing)
+import Data.Word (Word8)
+
 repeatedly :: Int -> a -> [a]
 repeatedly n item =
   let rfn m acc =
@@ -17,3 +20,55 @@ even1 n = n == 0 || odd1 (n - 1)
 
 odd1 :: Int -> Bool
 odd1 n = n /= 0 && even1 (n - 1)
+
+testone :: (Eq a, Num a) => a -> Bool
+testone 5 = True
+testone 6 = True
+testone a = False
+
+testtwo :: [a] -> [a]
+testtwo [] = []
+testtwo (x : xs) = xs
+
+data Color
+  = RGB Word8 Word8 Word8
+  deriving (Show)
+
+getBlue :: Color -> Word8
+getBlue (RGB r g b) = b
+
+main = putStrLn "Hello! play.hs"
+
+data Brightness
+  = Dark
+  | Bright
+
+data EightColor
+  = Black
+  | Red
+  | Green
+  | Yellow
+  | Blue
+  | Magenta
+  | Cyan
+  | White
+
+data AnsiColor
+  = AnsiColor Brightness EightColor
+
+ansiColorToVGA (AnsiColor Dark Black) = RGB 0 0 0
+ansiColorToVGA (AnsiColor Bright Black) = RGB 85 85 85
+ansiColorToVGA (AnsiColor Dark Red) = RGB 170 0 0
+ansiColorToVGA color =
+  case color of
+    AnsiColor Bright Red -> RGB 255 85 85
+    _ -> RGB 255 255 255
+
+isBright :: AnsiColor -> Bool
+isBright (AnsiColor Bright _) = True
+isBright _ = False
+
+isEmpty = isNothing . listToMaybe
+
+isEmpty2 [] = True
+isEmpty2 _ = False
